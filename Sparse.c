0e36153e2c6ddf84226815b6ef486ca624e34acf
rakshanpandian <92203280+rakshanpandian@users.noreply.github.com>
2025-11-19 23:32:00 +0530

    Update Sparse.c
    added count implimentation (it works it seems). Need to find a way to save it via the list

--- Sparse.c
+++ Sparse.c
@@ -2,14 +2,31 @@
 #include <stdlib.h>
 #include "Sparse.h" // Custom header file has all function defs and structures
 
+//Changes:
+//Stop loops and static objects
+
+//added count implimentation
+
+
 int find(SparseGrid *grid, int row, int col,int totrows, int totcols){
-	if (row < 0 || row >= totrows || col < 0 || col >= totcols) return 0;
+	if (col<0 || col>=totcols || row<0 || row>=totrows) return 0;
 	CellNode *temp=grid->col[col];
 	if (temp==NULL) return 0;
 	while (temp->down!=NULL && temp->row!=row){
 			temp=temp->down;
 	}
 	return (temp->row==row);
+}
+
+CellNode* find_cell(SparseGrid *grid, int row, int col,int totrows, int totcols){
+	if (col<0 || col>=totcols || row<0 || row>=totrows) return NULL;
+	CellNode *temp=grid->col[col];
+	if (temp==NULL) return NULL;
+	while (temp->down!=NULL && temp->row!=row){
+			temp=temp->down;
+	}
+	if (temp->row==row){ return temp;}
+    else{ return NULL;}
 }
 
 SparseGrid* initialize_grid(SparseGrid *grid, int m, int n) {
@@ -19,10 +36,11 @@ SparseGrid* initialize_grid(SparseGrid *grid, int m, int n) {
 	return grid;
 }
 
-CellNode* create_cell(int row, int col){
+CellNode* create_cell(int row, int col,int count){
 	CellNode *new_cell = (CellNode*) malloc(sizeof(CellNode));
     new_cell->row=row;
     new_cell->col=col;
+    new_cell->count=count;
     new_cell->right=NULL;
 	new_cell->left=NULL;
 	new_cell->up=NULL;
@@ -30,9 +48,9 @@ CellNode* create_cell(int row, int col){
 	return new_cell;
 }
 
-SparseGrid* add_cell(SparseGrid *grid, int row, int col,int totrows, int totcols) {
+SparseGrid* add_cell(SparseGrid *grid, int row, int col,int count,int totrows, int totcols) {
 	if (find(grid,row,col,totrows,totcols)) return grid;
-    CellNode *new_cell=create_cell(row,col);
+    CellNode *new_cell=create_cell(row,col,count);
 
     CellNode *current_row=grid->row[row];
     CellNode *current_col=grid->col[col];
@@ -96,23 +114,26 @@ SparseGrid* add_cell(SparseGrid *grid, int row, int col,int totrows, int totcols
 	return grid;
 }
 
+
 void remove_cell(SparseGrid *grid, int row, int col) {
-    CellNode *temp=grid->col[col];
-    if (temp!=NULL){
-        while (temp->row!=row && temp->down!=NULL) temp=temp->down;
-        if (temp!=NULL){
-            if (temp->row==row){
-                if (temp->left==NULL) grid->row[row]=temp->right;
-                else (temp->left)->right=temp->right;
-                if (temp->right!=NULL) (temp->right)->left=temp->left;
-                if (temp->up==NULL) grid->col[col]=temp->down;
-                else (temp->up)->down=temp->down;
-                if (temp->down!=NULL) (temp->down)->up=temp->up;
-                free(temp);
-                temp=NULL;
-            }
-        }
-    }
+	if (col>=0 && col<100 && row>=0 && row<100){
+		CellNode *temp=grid->col[col];
+		if (temp!=NULL){
+			while (temp->row!=row && temp->down!=NULL) temp=temp->down;
+			if (temp!=NULL){
+				if (temp->row==row){
+					if (temp->left==NULL) grid->row[row]=temp->right;
+					else (temp->left)->right=temp->right;
+					if (temp->right!=NULL) (temp->right)->left=temp->left;
+					if (temp->up==NULL) grid->col[col]=temp->down;
+					else (temp->up)->down=temp->down;
+					if (temp->down!=NULL) (temp->down)->up=temp->up;
+					free(temp);
+					temp=NULL;
+				}
+			}
+		}
+	}
 }
 
 int count_neighbors(SparseGrid *grid, int row, int col, int totrows, int totcols) {
@@ -135,19 +156,7 @@ void free_grid(SparseGrid *grid, int rows, int cols) {
 	for (int j=0;j<cols;j++) grid->col[j]=NULL;
 }
 
-SparseGrid* evolve_generation(SparseGrid *current_grid, int rows, int cols) {
-    SparseGrid *next_grid = initialize_grid(NULL, rows, cols);
-    int count;
-	int j;
-    for (int i = 0; i < rows; i++) {
-        for (j = 0; j < cols; j++) {
-            count = count_neighbors(current_grid, i, j,rows,cols);
-            if (count == 3 || (find(current_grid, i, j,rows,cols) && count == 2)) add_cell(next_grid, i, j,rows,cols);
-        }
-    }
-    return next_grid;
-}
-/*void print_grid(SparseGrid *grid, int rows, int cols) {
+void print_grid(SparseGrid *grid, int rows, int cols) {
     for (int i = 1; i < rows; i++) {
         CellNode *temp = grid->row[i];
         for (int j = 1; j < cols; j++) {
@@ -161,6 +170,132 @@ SparseGrid* evolve_generation(SparseGrid *current_grid, int rows, int cols) {
         printf("\n");
     }
 }
+
+SparseGrid* evolve_generation(SparseGrid *current_grid, int rows, int cols) {
+    SparseGrid *next_grid = initialize_grid(NULL, rows, cols);
+    int count;
+	int j;
+    CellNode *cell;
+    for (int i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+			//printf("%d",j);
+            count = count_neighbors(current_grid, i, j,rows,cols);
+            cell=find_cell(current_grid, i, j,rows,cols);
+            if (cell==NULL){
+                if (count==3) add_cell(next_grid, i, j,0,rows,cols);
+            }
+            else{
+                if (find(current_grid, i, j,rows,cols) && ((count == 2) || (count==3))) add_cell(next_grid, i, j,cell->count+1,rows,cols);
+            }
+			//printf("%d",count);
+
+        }
+		//printf("\n");
+    }
+    return next_grid;
+}
+
+
+
+ NODE* frontins(NODE *list,SparseGrid *x)
+  {
+	NODE *newnode;
+	  
+    newnode = (NODE *)(malloc(sizeof(NODE)));
+	newnode->info=x;
+	newnode->next=NULL;
+	
+	if(list==NULL)list=newnode;
+	else {
+		newnode->next=list;
+		list=newnode;
+	}
+	return list;
+  }
+  
+  
+NODE* frontdel(NODE *list, int ROWS, int COLS)
+  { NODE *temp;
+	if(list==NULL)printf("Empty List");
+	else {
+		temp=list;
+		list=list->next;
+		free_grid(temp->info, ROWS, COLS);
+		free(temp);
+	}
+	return list;
+  }
+  
+NODE* enddel(NODE *list, int ROWS, int COLS)
+  { NODE *temp, *prev;
+    temp=list;
+	if(list==NULL)printf("Empty List");
+	else if(list->next==NULL){list=NULL;
+	                          free(temp);
+							  }
+	else { 
+		while(temp->next!=NULL)
+		{   prev=temp;
+			temp=temp->next;
+		}
+		prev->next=NULL;
+		free_grid(temp->info, ROWS, COLS);
+		free(temp);
+	}
+	return list;
+  }
+  
+SparseGrid* endsearch(NODE *list)
+  { NODE *temp;
+    temp=list;
+	if(list==NULL)printf("Empty List");
+	else { 
+		while(temp->next!=NULL)
+		{
+			temp=temp->next;
+		}
+	}
+	return temp->info;
+  }
+  
+ 
+
+
+
+/*
+int find_duplicate(SparseGrid* curr_grid, SparseGrid* prevgrid, int rows, int cols,int totrows, int totcols){
+	return ((find(curr_grid,rows,cols) && find(prev_grid,rows,cols)) || (!find(curr_grid,rows,cols) && !find(prev_grid,rows,cols)));
+}
+
+int duplicate_neighbours(SparseGrid* curr_grid, SparseGrid* prevgrid, int rows, int cols,int totrows,int totcols){
+	int neighbours=find_duplicate(curr_grid, prevgrid, rows-1, cols-1,totrows,totcols)+find_duplicate(curr_grid, prevgrid, rows, cols-1,totrows,totcols)+find_duplicate(curr_grid, prevgrid, rows+1, cols-1,totrows,totcols)+find_duplicate(curr_grid, prevgrid, rows+1, cols,totrows,totcols)+find_duplicate(curr_grid, prevgrid, rows+1, cols+1,totrows,totcols)+find_duplicate(curr_grid, prevgrid, rows, cols+1,totrows,totcols)+find_duplicate(curr_grid, prevgrid, rows-1, cols+1,totrows,totcols)+find_duplicate(curr_grid, prevgrid, rows-1, cols,totrows,totcols)
+	return neighbours;
+}
+
+
+SparseGrid* remove_duplicate(SparseGrid *current_grid,SparseGrid *prev_grid, int rows, int cols) {
+    int count;
+	int j;
+    CellNode *cell;
+	CellNode *prev;
+    for (int i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+			//printf("%d",j);
+			found=(find(current_grid,i,j,rows,cols) && find(prev_grid,i,j,rows,cols))
+            if (found){
+				cell=find_cell(current_grid,i,j,rows,cols);
+				if (cell->count>2){
+					if (duplicate_neighbours(current_grid,prev_grid,i,j,rows,cols)==8) remove_cell(current_grid,i,j);
+				}
+			}
+			//printf("%d",count);
+
+        }
+		//printf("\n");
+    }
+    return current_grid;
+}*/
+/*
 int main(){
 	SparseGrid *grid;
 	grid=initialize_grid(grid,5,5);
