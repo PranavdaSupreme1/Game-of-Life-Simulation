422f2eca50cf505c2bd1ae6a325e4bfb884302e3
Pranav <Gonarchov@outlook.com>
2025-11-19 16:44:50 +0530

    Update Sparse.h
    minor changes

--- Sparse.h
+++ Sparse.h
@@ -1,19 +1,16 @@
 // Sparse.h - defining here all the functions and structs we'll be using for the Game of Life
-#ifndef SPARSE_H
-#define SPARSE_H
-
-#define SIZE 100
 
 //Data structures
 typedef struct CellNode {
     int row;
     int col;
+    int count;
     struct CellNode *right, *left, *up, *down;
 } CellNode;
 
 typedef struct {
-    CellNode *row[SIZE];
-    CellNode *col[SIZE];	// Head of a master linked list of all live cells, sorted
+    CellNode *row[100];
+    CellNode *col[100];	// Head of a master linked list of all live cells, sorted
 } SparseGrid;
 
 //Note: Only the functions used in RayGUI.c and Sparse.c are declared here.
@@ -23,5 +20,4 @@ void remove_cell(SparseGrid *grid, int row, int col); //Kills cell at (row,col).
 int find(SparseGrid *grid, int row, int col, int totrows, int totcols); //Returns 1 if cell at (row,col) is alive, else 0
 SparseGrid* evolve_generation(SparseGrid *current_grid, int rows, int cols); //Calculate next frame using Conway's rules
 void free_grid(SparseGrid *grid, int rows, int cols); //Kills all cells, clears sparse grid.
-
-#endif
+CellNode* find_cell(SparseGrid *grid, int row, int col ,int totrows, int totcols);
